5cad69b5563d99f1477805a3a1ab7fdaa0c727dd
peipeidong <peidzzz@163.com>
2020-04-23 10:48:07 +0800

    :wrench: updated config.

--- STM32F103xC/Src/main.c
+++ STM32F103xC/Src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 int main(){
+  // Add variable definition
   u8 str[] = "STM32F103xC Template by Peidzzz";
   // Add init here
   delay_init();
@@ -9,7 +10,7 @@ int main(){
   // Init end
   
   LCD_Clear(WHITE);
-	while(1){
+  while(1){
     /*** Add you own code here ***/
     LCD_ShowString(50, 0, 12*sizeof(str), 24, 24, str,  BLACK, WHITE);
     /*** Code end ***/
